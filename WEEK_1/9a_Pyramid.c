4e910689443ee3e578051a8e37df1c795e0c5db1
GuruNadH_Pukkalla <53335728+GuruNadh552@users.noreply.github.com>
2019-11-09 12:07:20 +0530

    Create 9a_Pyramid.c

---
+++ WEEK_1/9a_Pyramid.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+  Code By Guru
+*/
+int main()
+{
+    int i, s, r, k=0;
+    printf("Enter number of rows: ");
+    scanf("%d",&r);
+    for(i=1; i<=r; ++i, k=0)
+    {
+        for(s=1; s<=r-i; ++s)
+        {
+            printf("  ");
+        }
+        while(k != 2*i-1)
+        {
+            printf("* ");
+            ++k;
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
